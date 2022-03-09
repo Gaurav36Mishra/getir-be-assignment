3e349da10de52d759d66339df78f56fd6c156be3
Gaurav36Mishra <gaurav05mishra@gmail.com>
2022-03-09 13:22:02 +0530

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
-    const DB_CONNECTION = process.env.DB_CONNECTION
+    const DB_CONNECTION = process.env.DB_CONNECTION || "mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true"
     return await mongoose.connect(DB_CONNECTION)
 }
 
