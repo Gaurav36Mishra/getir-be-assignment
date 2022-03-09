aa3d09536cd4b2296fb2d71a07febb6921c9bed3
Gaurav36Mishra <gaurav05mishra@gmail.com>
2022-03-09 13:21:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ dotenv.config()
 
 const { connectDB } = require('./database/connection')
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080
 
 connectDB().then(() => {
     console.log('DB connection successful')
